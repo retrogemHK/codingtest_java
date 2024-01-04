050950eb45457a10e0e4a04e582bde53b287e344
retrogemHK <kylados@naver.com>
2024-01-05 06:36:40 +0900

    add

--- solution/61.java
+++ solution/61.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Code552A {
+public class Solution {
 
     public static void main(String[] args) {
         System.out.println(Arrays.deepToString(solution(3)));
